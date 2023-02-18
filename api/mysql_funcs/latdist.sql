72fb5b42d39c50dbb819561d0a10e91912d04764
nosharp <contact@nosharp.cc>
2023-02-18 20:01:04 +0000

    feat: add haversine distance method

---
+++ api/mysql_funcs/latdist.sql
@@ -0,0 +1,33 @@
+
+-- https://www.movable-type.co.uk/scripts/latlong.html
+CREATE OR REPLACE FUNCTION haversine_distance(
+     lat_1 DOUBLE,
+     lon_1 DOUBLE,
+     lat_2 DOUBLE,
+     lon_2 DOUBLE )
+RETURNS DOUBLE
+BEGIN
+    DECLARE earthRadius DOUBLE;
+    DECLARE pi180 DOUBLE;
+    DECLARE lat1Pi180 DOUBLE;
+    DECLARE lat2Pi180 DOUBLE;
+    DECLARE latDiff DOUBLE;
+    DECLARE lonDiff DOUBLE;
+
+    DECLARE a DOUBLE;
+    DECLARE c DOUBLE;
+    SET pi180 = PI() / 180;
+    SET earthRadius = 6371e3;
+    SET lat1Pi180 = lat_1 * pi180;
+    SET lat2Pi180 = lat_2 * pi180;
+
+    SET latDiff = (lat_2 - lat_1) * pi180;
+    SET lonDiff = (lon_2 - lon_1) * pi180;
+
+    SET a = sin(latDiff/2) * sin(lonDiff / 2) + cos(lat1Pi180) * cos(lat2Pi180) * sin(lonDiff/2) * sin(lonDiff/2);
+    SET c = 2 * atan2(sqrt(a), sqrt(1-a));
+
+    RETURN (earthRadius * c) / 3;
+end;
+
+SELECT distance(52.5144668025837, -1.9688887936005763, 51.94434914229727, 5.308123255734208);
