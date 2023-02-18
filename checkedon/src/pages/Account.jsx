205973d235fe9f1fd8646d0594fe9df9474faab5
Ethan Egerton <38867954+ethan-egerton@users.noreply.github.com>
2023-02-18 23:17:31 +0000

    feat: add redirect if not logged in

--- checkedon/src/pages/Account.jsx
+++ checkedon/src/pages/Account.jsx
@@ -1,22 +1,30 @@
 import React, { useState } from 'react';
-import 'react-phone-number-input/style.css'
 import PhoneInput from 'react-phone-number-input';
+import Cookies from 'js-cookie';
+import "../index.css";
+import 'react-phone-number-input/style.css'
 
 const user = "Ethan";
 
 function Account() {
-    const [value, setValue] = useState('');
+    const accessToken = Cookies.get('access_token');
+    if (!accessToken) {
+        window.location.replace("#/login");
+    }
 
+    const [value, setValue] = useState('');
+    
     const sendPhoneNumber = () => {
+        console.log(value);
     }
 
     return (
-        <>
+        <section>
             <h2>Hello {user}</h2>
             <p>Please input your phone number</p>
             <PhoneInput className='' placeholder="Enter phone number" value={value} onChange={setValue} style={{width: "250px"}} />
             <button onClick={sendPhoneNumber}>Save</button>
-        </>
+        </section>
     );
 }
 
