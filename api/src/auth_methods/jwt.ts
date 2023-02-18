34b0a04bf89663c2d00cfbbd4f4d05990d006a54
nosharp <contact@nosharp.cc>
2023-02-18 18:56:21 +0000

    feat: pivot to JWT

---
+++ api/src/auth_methods/jwt.ts
@@ -0,0 +1,31 @@
+import { Envuments } from "envuments";
+import passport from "passport";
+import { Strategy, ExtractJwt } from "passport-jwt";
+import user from "../controllers/user";
+
+passport.use(
+  new Strategy(
+    {
+      jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme("jwt"),
+      secretOrKey: Envuments.get("JWT_SECRET"),
+      issuer: "api.checkedon.tech",
+      audience: "api.checkedon.tech",
+    },
+    async (jwtPayload, cb) => {
+      console.log(jwtPayload);
+      const foundUser = await user.findFirst({
+        where: {
+          id: jwtPayload.userId,
+        },
+      });
+
+      if (foundUser == null) {
+        return cb(null, false);
+      }
+
+      return cb(null, foundUser, jwtPayload);
+    }
+  )
+);
+
+export default 1;
