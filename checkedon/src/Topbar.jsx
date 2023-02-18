90034bffbef364b8811f5e5b3173d67323cb87da
Ethan Egerton <38867954+ethan-egerton@users.noreply.github.com>
2023-02-18 16:34:49 +0000

    feat: add login or account button

--- checkedon/src/Topbar.jsx
+++ checkedon/src/Topbar.jsx
@@ -1,13 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const user = "lol";
+const user = "lmao";
+const signedIn = true;
+
+class LoginControl extends React.Component {
+    render() {
+        let output;
+        if (signedIn) {
+            output = <><p>Welcome {user}!</p><button>Your Account</button></>
+        } else {
+            output = <Link href="/login"><button>Sign Up!</button></Link>
+        }
+
+        return (
+            <>
+                {output}
+            </>
+        )
+    }
+}
 
 function Topbar() {
     return (
         <div>
             <h1>CheckedOn</h1>
-            <p>Welcome {user}</p>
+            <LoginControl />
             <nav>
                 <Link to="/">Home</Link>
                 <Link to="/setup">Setup</Link>
