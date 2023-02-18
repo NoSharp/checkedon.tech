880d7adf21e0d4675687b9b1782c2c923ff3c44c
Ethan Egerton <38867954+ethan-egerton@users.noreply.github.com>
2023-02-18 22:19:12 +0000

    fix: change google auth url

--- checkedon/src/pages/Login.jsx
+++ checkedon/src/pages/Login.jsx
@@ -7,7 +7,7 @@ function Login() {
         <section>
             <h2>Login</h2>
             <p>Please login using Google</p>
-            <a target="_blank" rel='noreferrer' href="https://api.checkedon.tech/auth/method/google"><button className='login-button'>Log in with google</button></a>
+            <a target="_blank" rel='noreferrer' href="https://checkedon.tech/api/auth/method/google"><button className='login-button'>Log in with google</button></a>
         </section>
     );
 }
