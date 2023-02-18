ae6910a0ad2a83a5df95df60573269fe9b0ab355
Ethan Egerton <38867954+ethan-egerton@users.noreply.github.com>
2023-02-18 16:32:23 +0000

    feat: add account page

---
+++ checkedon/src/pages/Account.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import 'react-phone-number-input/style.css'
+import PhoneInput from 'react-phone-number-input';
+
+const user = "Ethan";
+
+function Account() {
+    const [value, setValue] = useState('');
+
+    const sendPhoneNumber = () => {
+        console.log(value)
+    }
+
+    return (
+        <>
+            <h2>Hello {user}</h2>
+            <p>Please input your phone number</p>
+            <PhoneInput className='' placeholder="Enter phone number" value={value} onChange={setValue} style={{width: "250px"}} />
+            <button onClick={sendPhoneNumber}>Save</button>
+        </>
+    );
+}
+
+export default Account;
