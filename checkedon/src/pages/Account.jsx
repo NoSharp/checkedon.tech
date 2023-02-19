f791dc901ff9c0c876bfb75df58f64550e4c7d04
Ethan Egerton <38867954+ethan-egerton@users.noreply.github.com>
2023-02-19 03:17:26 +0000

    feat: add welcome name

--- checkedon/src/pages/Account.jsx
+++ checkedon/src/pages/Account.jsx
@@ -1,10 +1,31 @@
 import React, { useState } from 'react';
 import PhoneInput from 'react-phone-number-input';
 import Cookies from 'js-cookie';
+import getUserData from "../apiHandler";
 import "../index.css";
 import 'react-phone-number-input/style.css'
 
-const user = "Ethan";
+class HelloName extends React.Component {
+    state = {
+        userName: null
+    };
+
+    componentDidMount(){
+        getUserData().then((userName)=>{
+            this.setState({
+                userName: userName
+            })
+        });
+    }
+
+    render () {
+        const username = this.state.userName;
+        const splitUsername = username.split(/[ ,]+/);
+        return (
+            <h2>Hello {splitUsername[0]}!</h2>
+        )
+    }
+}
 
 function Account() {
     const accessToken = Cookies.get('access_token');
@@ -20,7 +41,7 @@ function Account() {
 
     return (
         <section>
-            <h2>Hello {user}</h2>
+            <HelloName />
             <p>Please input your phone number</p>
             <PhoneInput className='' placeholder="Enter phone number" value={value} onChange={setValue} style={{width: "250px"}} />
             <button onClick={sendPhoneNumber}>Save</button>
